eaf9226cbc59ecf220032d1e8b838fcd9f7dc49f
cmdspeed <cmdspeed@gmail.com>
2023-03-04 15:53:12 +0100

    task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+import * as throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+let time = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(time);
+
+const onPlay = function (data) {
+  const secondPlay = data.seconds;
+  localStorage.setItem('videoplayer-current-time', secondPlay);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
