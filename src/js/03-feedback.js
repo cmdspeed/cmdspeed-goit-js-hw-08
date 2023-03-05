15fad57f65bea6a84c0ea5cda87a274dc983e437
cmdspeed <cmdspeed@gmail.com>
2023-03-05 12:15:11 +0100

    end task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
 
+import * as throttle from 'lodash.throttle';
+
+const formValue = {};
+
+form.addEventListener('input', throttle(setLocalStorage, 500));
+form.addEventListener('submit', submitForm);
+
+getLocalStorageAndSet();
+
+function setLocalStorage(event) {
+  formValue[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formValue));
+}
+
+function getLocalStorageAndSet() {
+  const storage = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (storage) {
+    email.value = storage.email;
+    message.value = storage.message;
+  }
+}
+
+function submitForm(event) {
+  const storage = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log(`email: ${storage.email} message: ${storage.message}`);
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
